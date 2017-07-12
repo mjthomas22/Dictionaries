ecb911da0ef0b26d9644d0f210d1818d626265b4
mjthomas22 <mthomas22@gmail.com>
2017-07-12 14:18:14 -0400

    add two dicitionaries

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -10,6 +10,42 @@ namespace Dictionaries
     {
         static void Main(string[] args)
         {
+            ////syntax for dictionary
+
+            //Dictionary<string, string> snackMachine = new Dictionary<string, string>()
+            //{
+            //    {"A1","Twix" },
+            //    {"A2","Twizzlers" },
+            //    {"A3", "Reeses" },
+            //    {"A4", "Sourpatch Kids" }
+
+            //};
+
+            //foreach (KeyValuePair<string,string>item in snackMachine)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+            ////using add method to add drinks
+            //drinkMachine.Add(10, "Ginger Ale");
+            //drinkMachine.Add(11, "Bottled Water");
+            //drinkMachine.Add(12, "Orange");
+            //drinkMachine.Add(13, "Cherry Coke");
+            //drinkMachine.Add(14, "Mountain Dew Code Red");
+
+            //Console.WriteLine(drinkMachine.Count);
+
+            //foreach (KeyValuePair<int,string> drink in drinkMachine)
+            //{               // drink .Value would return the values
+
+            //    Console.WriteLine(drink.Key);
+            //    Console.WriteLine(drink.Value);
+            //}
+
+
+
+
         }
     }
 }
